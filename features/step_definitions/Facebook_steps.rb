bdd38d4c84e9d2c8720866d19212f0765b647934
Chuck van der Linden <sqapro@gmail.com>
2012-05-14 19:22:13 -0700

    Added step file with snippets in step definitions directory

---
+++ features/step_definitions/Facebook_steps.rb
@@ -0,0 +1,13 @@
+
+
+Given /^I am logged into facebook$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I update my status$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a status update should be posted$/ do
+  pending # express the regexp above with the code you wish you had
+end
